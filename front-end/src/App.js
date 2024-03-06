8b628495c33d1a9886c2ebeb20813f1b7be7d9ad
--replace-all <ylei0016@student.monash.edu>
2024-03-06 17:31:59 +1100

    initial project

---
+++ front-end/src/App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter } from "react-router-dom";
+import Main from "./components/Main";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Main></Main>
+    </BrowserRouter>
+  );
+}
+
+export default App;
